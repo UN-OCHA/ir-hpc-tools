ba30c5e0d5aad9e3723434e0bfa3e9d082c4924e
Peter Droogmans (attiks) <peter@attiks.com>
2018-02-15 10:45:27 +0100

    Find using regexp

--- scripts/review.sh
+++ scripts/review.sh
@@ -1,6 +1,6 @@
-test ! -d ./html/sites/all/modules/custom || find -L ./html/sites/all/modules/custom -name '*' -print0 | xargs -0 -n 1 -P 4 php -l
-test ! -d ./html/sites/all/modules/features || find -L ./html/sites/all/modules/features -name '*' -print0 | xargs -0 -n 1 -P 4 php -l
-test ! -d ./html/sites/all/themes/custom || find -L ./html/sites/all/themes/custom -name '*' -print0 | xargs -0 -n 1 -P 4 php -l
+test ! -d ./html/sites/all/modules/custom || find -L ./html/sites/all/modules/custom -iregex '.*\.\(php\|module\|inc\|install\)$' -print0 | xargs -0 -n 1 -P 4 php -l
+test ! -d ./html/sites/all/modules/features || find -L ./html/sites/all/modules/features -iregex '.*\.\(php\|module\|inc\|install\)$' -print0 | xargs -0 -n 1 -P 4 php -l
+test ! -d ./html/sites/all/themes/custom || find -L ./html/sites/all/themes/custom -iregex '.*\.\(php\|module\|inc\|install\)$' -print0 | xargs -0 -n 1 -P 4 php -l
 
 ./bin/phpcs --config-set installed_paths vendor/drupal/coder/coder_sniffer
 test ! -d ./html/sites/all/modules/custom || ./bin/phpcs -p --report=full ./html/sites/all/modules/custom
