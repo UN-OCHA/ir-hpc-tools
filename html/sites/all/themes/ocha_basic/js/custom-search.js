fb83af77eac109aee782b1ad55a4601ad2b631a9
left23 <lcdemc@gmail.com>
2018-09-07 17:12:25 +0200

    update search js to reflect recent changes to ocha_basic

--- html/sites/all/themes/ocha_basic/js/custom-search.js
+++ html/sites/all/themes/ocha_basic/js/custom-search.js
@@ -6,12 +6,17 @@
 (function ($, Drupal) {
   Drupal.behaviors.cdSearch = {
     attach: function (context, settings) {
-      $('.cd-search__input').on('focus', function (e) {
-        $(this).parents().find('.cd-search__submit').toggleClass('js-has-focus');
+      // Apply focus to input when dropdown is shown.
+      $('.cd-search').on('shown.bs.dropdown', function () {
+        $(this).find('#cd-search').focus();
+      }).on('hidden.bs.dropdown', function () {
+        $(this).find('#cd-search').blur();
       });
 
-      $('.cd-search__input').on('blur', function (e) {
-        $(this).parents().find('.cd-search__submit').toggleClass('js-has-focus');
+      $('.cd-search__input').on('focus', function (e) {
+        $(this).parents('.cd-search__form-inner').find('.cd-search__submit').addClass('js-has-focus');
+      }).on('blur', function (e) {
+        $(this).parents('.cd-search__form-inner').find('.cd-search__submit').removeClass('js-has-focus');
       });
     }
   };
